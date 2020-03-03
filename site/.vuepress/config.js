d49d9db37138b689d5013ba28939aaa45bc9e4da
Chris Waring <chris@wwaves.co>
2020-03-03 15:20:45 +0000

    add clean urls plugin

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -1,4 +1,14 @@
 module.exports = {
   title: 'Hello VuePress',
-  description: 'Just playing around'
+  description: 'Just playing around',
+  plugins: [
+    [
+      'vuepress-plugin-clean-urls',
+      {
+        normalSuffix: '/',
+        indexSuffix: '/',
+        notFoundPath: '/404.html'
+      }
+    ]
+  ]
 }
